63f2e99b828fd0d58c3f8e68cf1b542c174e6f6f
조준희 <46863381+RedJunHee@users.noreply.github.com>
2023-11-20 12:28:00 +0900

    [init] Project Init

---
+++ src/test/java/my/modulesource/aop_log/AopLogApplicationTests.java
@@ -0,0 +1,13 @@
+package my.modulesource.aop_log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
